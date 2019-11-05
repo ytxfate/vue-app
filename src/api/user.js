6177034185e861fab26f9ea1775e6f92ada88bcc
hsd <xiivixxiv520@foxmail.com>
2019-11-05 11:38:38 +0800

    [IMP] 添加接口注释

--- src/api/user.js
+++ src/api/user.js
@@ -1,14 +1,24 @@
 import request from '@/utils/request'
 
-export function login (data) {
+export function login (data) { // 用户登录
   return request({
     url: '/user/login',
     method: 'post',
     data
   })
 }
+// 登录接口返回示例
+// {
+//   "code": 200,
+//   "isSuccess": true,
+//   "msg": "请求成功",
+//   "response": {
+//     "jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NzI5MjYyNzguMCwiaWF0IjoxNTcyOTI0NDc4LjAsImlzcyI6ImhzZCIsImRhdGEiOnsidXNlcm5hbWUiOiJhZG1pbiJ9fQ.Vc6QbCQNLYtVLQ6JMdIKxlnqELc2ZOFVZV_rTP4NJIw",
+//     "refresh_jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NzI5MjgwNzguMCwiaWF0IjoxNTcyOTI0NDc4LjAsImlzcyI6ImhzZCIsImRhdGEiOnt9fQ.GDC3JLuuBf26A_RkpxChrpKIF-7xKqmUyw761l-tz3c"
+//   }
+// }
 
-export function getRouters (data) {
+export function getRouters (data) { // 获取用户 routers
   return request({
     url: '/user/get_routers',
     method: 'post',
@@ -16,10 +26,20 @@ export function getRouters (data) {
   })
 }
 
-export function refreshToken (data) {
+export function refreshToken (data) { // 刷新 jwt 信息
   return request({
     url: '/user/refresh_login_status',
     method: 'post',
     data
   })
 }
+// Token 刷新接口返回示例
+// {
+//   "code": 200,
+//   "isSuccess": true,
+//   "msg": "请求成功",
+//   "response": {
+//     "jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NzI5MjYyNzguMCwiaWF0IjoxNTcyOTI0NDc4LjAsImlzcyI6ImhzZCIsImRhdGEiOnsidXNlcm5hbWUiOiJhZG1pbiJ9fQ.Vc6QbCQNLYtVLQ6JMdIKxlnqELc2ZOFVZV_rTP4NJIw",
+//     "refresh_jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NzI5MjgwNzguMCwiaWF0IjoxNTcyOTI0NDc4LjAsImlzcyI6ImhzZCIsImRhdGEiOnt9fQ.GDC3JLuuBf26A_RkpxChrpKIF-7xKqmUyw761l-tz3c"
+//   }
+// }
